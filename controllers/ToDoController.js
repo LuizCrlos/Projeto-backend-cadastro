e71be3f0ee5205ea73ccb3eccb2637ac3cc02707
Luiz <seu-email@exemplo.com>
2024-05-28 22:28:28 -0300

    Primeiro commit

---
+++ controllers/ToDoController.js
@@ -0,0 +1,38 @@
+const ToDoModel = require("../models/ToDoModel");
+
+module.exports.getToDo = async (req, res) => {
+  const toDo = await ToDoModel.find();
+  res.send(toDo);
+};
+
+module.exports.saveToDo = async (req, res) => {
+  const { text } = req.body;
+
+  ToDoModel.create({ text }).then((data) => {
+    console.log("Os dados foram salvos dentro do Banco de Dados...");
+    console.log(data);
+    res.send(data);
+  });
+};
+
+module.exports.updateToDo = async (req, res) => {
+  const { _id, text } = req.body;
+  ToDoModel.findByIdAndUpdate(_id, { text })
+    .then(() => {
+      res.send("Os dados foram atualizados...");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports.deleteToDo = async (req, res) => {
+  const { _id } = req.body;
+  ToDoModel.findByIdAndDelete(_id)
+    .then(() => {
+      res.send("Os dados foram deletados...");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
